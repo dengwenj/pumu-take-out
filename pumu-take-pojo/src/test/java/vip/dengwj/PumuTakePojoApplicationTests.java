10c907f98bc05f01a2c8871759549a7f61948a69
朴睦 <927561237@qq.com>
2024-05-29 00:28:33 +0800

    项目环境架构搭建

---
+++ pumu-take-pojo/src/test/java/vip/dengwj/PumuTakePojoApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.dengwj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumuTakePojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ pumu-take-service/src/main/java/vip/dengwj/PumuTakeServiceApplication.java
@@ -0,0 +1,12 @@
+package vip.dengwj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumuTakeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PumuTakeServiceApplication.class, args);
+    }
+}
