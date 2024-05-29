d5d7c5a1454736ef39f632bb6336a0f25c5fac07
朴睦 <927561237@qq.com>
2024-05-29 23:28:16 +0800

    接口文档

---
+++ pumu-take-service/src/main/java/vip/dengwj/interceptor/JwtTokenAdminInterceptor.java
@@ -0,0 +1,51 @@
+package vip.dengwj.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import vip.dengwj.constant.JWTConstant;
+import vip.dengwj.properties.JWTProperties;
+import vip.dengwj.utils.JWTUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * jwt令牌校验的拦截器
+ */
+@Component
+@Slf4j
+public class JwtTokenAdminInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private JWTProperties jwtProperties;
+
+    // 校验jwt
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //判断当前拦截到的是Controller的方法还是其他资源
+        if (!(handler instanceof HandlerMethod)) {
+            //当前拦截到的不是动态方法，直接放行
+            return true;
+        }
+
+        //1、从请求头中获取令牌
+        String token = request.getHeader(jwtProperties.getAdminTokenName());
+
+        //2、校验令牌
+        try {
+            Map<String, Object> map = JWTUtils.parseJWT(jwtProperties.getAdminSecretKey(), token);
+            Long empId = Long.valueOf(map.get(JWTConstant.EMP_ID).toString());
+            log.info("当前员工id：{}", empId);
+            //3、通过，放行
+            return true;
+        } catch (Exception ex) {
+            //4、不通过，响应401状态码
+            response.setStatus(401);
+            return false;
+        }
+    }
+}
