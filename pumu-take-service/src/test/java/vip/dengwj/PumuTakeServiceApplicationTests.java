0b5a0cdcef596b41adc75b187cb542f6596448df
朴睦 <927561237@qq.com>
2024-06-02 23:48:38 +0800

    spring 操作 redis hash

--- pumu-take-service/src/test/java/vip/dengwj/PumuTakeServiceApplicationTests.java
+++ pumu-take-service/src/test/java/vip/dengwj/PumuTakeServiceApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.*;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -44,5 +46,31 @@ class PumuTakeServiceApplicationTests {
         redisTemplate.opsForValue().setIfAbsent("pp", "皮皮");
         System.out.println(redisTemplate.opsForValue().get("pp"));
         redisTemplate.opsForValue().setIfAbsent("pp", "啤啤");
+    }
+
+    /**
+     * spring 操作 redis hash
+     */
+    @Test
+    public void testHash() {
+        // hset hget hdel hkeys hvals
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+        // 设置
+        hashOperations.put("hash", "key1", "value1");
+        hashOperations.put("hash", "key2", "value2");
+
+        // 获取
+        System.out.println(hashOperations.get("hash", "key1"));
+
+        // 删除
+        hashOperations.delete("hash", "key2");
+
+        // 获取这个 key 的 hashKey
+        Set<Object> student = hashOperations.keys("hash");
+        System.out.println(student);
+
+        // 获取这个 key 的 hashVal
+        List<Object> student1 = hashOperations.values("hash");
+        System.out.println(student1);
     }
 }
