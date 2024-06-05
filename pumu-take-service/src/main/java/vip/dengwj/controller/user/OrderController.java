1c5ba152656d63ec5f87f911e731576e03615410
朴睦 <927561237@qq.com>
2024-06-05 19:19:43 +0800

    模拟支付成功

--- pumu-take-service/src/main/java/vip/dengwj/controller/user/OrderController.java
+++ pumu-take-service/src/main/java/vip/dengwj/controller/user/OrderController.java
@@ -34,9 +34,14 @@ public class OrderController {
     @PutMapping("/payment")
     @ApiOperation("订单支付")
     public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
-        log.info("订单支付：{}", ordersPaymentDTO);
-        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
-        log.info("生成预支付交易单：{}", orderPaymentVO);
-        return Result.success(orderPaymentVO);
+        //log.info("订单支付：{}", ordersPaymentDTO);
+        //OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+        //log.info("生成预支付交易单：{}", orderPaymentVO);
+        //return Result.success(orderPaymentVO);
+
+        // 模拟支付成功
+        //业务处理，修改订单状态、来单提醒
+        orderService.paySuccess(ordersPaymentDTO.getOrderNumber());
+        return Result.success(new OrderPaymentVO());
     }
 }
